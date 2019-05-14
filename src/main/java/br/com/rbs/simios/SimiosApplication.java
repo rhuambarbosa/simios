c54898a675ce86ef0f1f36b4ce34b6c14902695e
rhuambarbosa <rsousa@e-safer.com.br>
2019-05-14 11:57:57 -0300

    Criaçã da estruturação inicial do projeto.

---
+++ src/main/java/br/com/rbs/simios/SimiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rbs.simios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiosApplication.class, args);
+	}
+
+}
