e0373c93c28cc1fbf2e00f2fdcf2e960408eaac2
rhuambarbosa <rsousa@e-safer.com.br>
2019-05-14 12:33:27 -0300

    Configurações iniciais; Inclusão de index na criação da tabela;

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE dna_bank (
 	id numeric PRIMARY KEY,
+	dna_idx text NOT null,
 	dna text NOT null,
 	simian BOOL NOT null default false,
 	creation_date TIMESTAMP NOT NULL default now()
 );
+CREATE index idx_dna_idx ON dna_bank ((upper(dna_idx)));
 
 CREATE SEQUENCE sq_human_dna;
 
